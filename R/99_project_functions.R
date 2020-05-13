384c52fb4c9d2241427e2002b34a6026c06da5f0
zidibot <gvladja@gmail.com>
2020-05-13 16:00:24 +0000

    Restructured the plotting_boxplot() and added a comment

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -25,8 +25,13 @@ get_legend <- function(myggplot){
 
 
 # Customized boxplot ------------------------------------------------------
-
-plotting_boxplot <- function(data, subset_term, color, 
+# The function allows to split the data into individual subsets while taking 
+# a reference range for the "rectangle" object in the plot.
+# This structure was necessary to communicate the message clearly, with plots
+# placed side by side and having one shared legend to preserve space. 
+# facet_*() could not achieve the desired effect (distinct patient IDs on y.ax) 
+plotting_boxplot <- function(data, subset_term, 
+                             color, 
                              control_range = control_range, 
                              control_range_colour = "yellow1") {
   plot <- data %>%   
@@ -35,69 +40,20 @@ plotting_boxplot <- function(data, subset_term, color,
            Class) %>%
     subset(Class == subset_term) %>%
     pivot_longer(cols = starts_with('NP_')) %>%
-    ggplot(mapping = aes(y = reorder(patient_ID, value, FUN = median),
-                         x = value)) + 
-    annotate("rect", 
-             xmin = control_range[1], 
-             xmax = control_range[2], # supply from data
-             ymin = -Inf,
-             ymax = Inf,
-             alpha = 0.3, 
-             fill = control_range_colour) +
-    geom_boxplot(alpha = 0.7,
-                 varwidth = TRUE,
-                 outlier.shape = NA,
-                 fill = color) + 
-    labs(x = "Log2 Expression levels",
-         y = "Patients",
-         title = subset_term) +
-    xlim(-10, 10) +
-    geom_vline(aes(xintercept = control_range[2],
-                   color = "Control samples"), 
-               linetype = "solid",
-               size = 1) +
-    geom_vline(aes(xintercept = control_range[1]), 
-               linetype = "solid",
-               size=1) +
-    scale_color_manual(name = "Inter Quartile Range:", 
-                       values = c('Control samples' = "black", 
-                                  'Lower Bound' = "grey12")) +
-    stat_summary(fun = "median", 
-                 geom = "point", 
-                 colour = "white", 
-                 shape = 23, 
-                 size = 1) +
-    theme_bw(base_family = "Times", 
-             base_size = 16) +
-    theme(legend.position = "bottom",
-          legend.title = element_text(size = 15),
-          axis.text.x = element_text(size = 18),
-          axis.text.y = element_blank(),
-          plot.title = element_text(hjust = 0.5,
-                                    size = 22))
-
-  return(plot)
-}
-
-# TESTING
-plotting_boxplot <- function(data, subset_term, color, 
-                              control_range = control_range, 
-                              control_range_colour = "yellow1") {
-  plot <- data %>%   
-    select(patient_ID,
-           starts_with("NP_"),
-           Class) %>%
-    subset(Class == subset_term) %>%
-    pivot_longer(cols = starts_with('NP_')) %>%
+    drop_na() %>%
     ggplot() + 
-    geom_rect(aes(xmin = control_range[1],
+    geom_rect(aes(xmin = control_range[1], # supply from data
                   xmax = control_range[2], # supply from data
-                  ymin = -Inf, ymax = Inf, 
-                  color = "Controls"),
+                  ymin = -Inf, ymax = Inf, # to fit the plot height
+                  color = "Controls"),     # for legend display
               fill = "yellow2",
               alpha = 0.3) +
     geom_boxplot(mapping = aes(y = reorder(patient_ID, value, FUN = median),
-                               x = value),fill=color,alpha = 0.7, varwidth = TRUE, outlier.shape = NA) +
+                               x = value),
+                 fill=color, 
+                 alpha = 0.7, 
+                 varwidth = TRUE, 
+                 outlier.shape = NA) +
     scale_color_manual('Inter Quartile Range',
                        values = 'yellow',  
                        guide = guide_legend(override.aes = list(alpha = 0.1))) +
@@ -107,23 +63,23 @@ plotting_boxplot <- function(data, subset_term, color,
     geom_vline(aes(xintercept = control_range[1]), 
                linetype = "solid",
                size=1) +
-    # stat_summary(fun = "median", 
-    #              geom = "point", 
-    #              colour = "white", 
-    #              shape = 23, 
-    #              size = 1) +
+    stat_summary(fun = "median",
+                 geom = "point",
+                 colour = "white",
+                 shape = 23,
+                 size = 0.75) +
     labs(x = "Log2 Expression levels",
          y = "Patients",
          title = subset_term) +
     xlim(-10, 10) +
     theme_bw(base_family = "Times", 
-             base_size = 16) +
+             base_size = 14) +
     theme(legend.position = "bottom",
           legend.title = element_text(size = 15),
-          axis.text.x = element_text(size = 18),
+          axis.text.x = element_text(size = 12),
           axis.text.y = element_blank(),
           plot.title = element_text(hjust = 0.5,
-                                    size = 22))
+                                    size = 20))
   
   return(plot)
 }
